4ba14e1d6ee6b1ccd7725ba73b6db59ce3b1190f
Mike <michael.hartman0@gmail.com>
2020-11-23 11:45:58 -0500

    Create scan.ps1

---
+++ scan.ps1
@@ -0,0 +1,56 @@
+Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force
+
+Write-Host (
+    "
+     ================================
+     ================================
+     ====Starting Disk Space Scan====
+     ================================
+     ================================
+    "
+)
+
+Start-Sleep -Seconds 4
+if ( -not (Test-Path 'C:\temp\diskspacereports\Servers.txt' -PathType Leaf))
+{
+    # the servers.txt file wasn't found, write error and exit
+    "No server text file found. Please create a Servers.txt at c:\temp\diskspacereports\ or edit script to look at desired path."
+    exit
+}
+    else {
+        # servers.txt exists, grab it
+        $File = Get-Content -Path C:\temp\diskspacereports\servers.txt
+        Write-Host "Retrieving contents of servers.txt"
+        Start-Sleep -Seconds 4
+    }
+
+function Get-Servers {
+    $Servers = Get-Content C:\temp\diskspacereports\servers.txt
+    $ServerCount = $Servers.count
+    Write-Host "Found " $ServerCount "server(s) in text file"
+    Start-Sleep -Seconds 4  
+}
+Get-Servers
+
+$RunAccount = Get-Credential -Message "Enter admin account username and password"
+$LogDate = get-date -f yyyy_MM_dd_hhmm
+$DiskReport = ForEach ($Servernames in ($File))
+
+
+{Get-WmiObject win32_logicaldisk -Credential $RunAccount `
+-ComputerName $Servernames -Filter "Drivetype=3" `
+-ErrorAction SilentlyContinue }
+
+#create reports
+$DiskReport | 
+Select-Object @{Label = "Server Name";Expression = {$_.SystemName}},
+@{Label = "Drive Letter";Expression = {$_.DeviceID}},
+@{Label = "Total Capacity (MB)";Expression = {"{0:N1}" -f( $_.Size / 1mb)}},
+@{Label = "Free Space (MB)";Expression = {"{0:N1}" -f( $_.Freespace / 1mb ) }},
+@{Label = 'Free Space (%)'; Expression = {"{0:P0}" -f ($_.freespace/$_.size)}} |
+
+#Export report to CSV file (Disk Report)
+Export-Csv -path "C:\temp\diskspacereports\DiskReport\DiskReport_$logDate.csv" -NoTypeInformation
+
+#Open csv file
+Invoke-Item C:\Temp\DiskSpaceReports\DiskReport\DiskReport_$logDate.csv
