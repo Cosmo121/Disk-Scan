6dd88d4724347f611a21aa270f4b1e9a506e367c
Mike <michael.hartman0@gmail.com>
2020-11-24 17:27:46 -0500

    Update scan.ps1
    Changed function Get-Servers to be easier to edit. Got rid of $Servers.count

--- scan.ps1
+++ scan.ps1
@@ -1,4 +1,3 @@
-Set-ExecutionPolicy -Scope CurrentUser Unrestricted -Force
 
 Write-Host (
     "
@@ -10,43 +9,54 @@ Write-Host (
     "
 )
 
-Start-Sleep -Seconds 4
-if ( -not (Test-Path 'C:\temp\diskspacereports\Servers.txt' -PathType Leaf))
+
+function Get-Servers {
+    
+    param (
+        $TxtFilePath = 'C:\temp\diskspacereports\servers.txt'
+    )
+
+    $Servers = Get-Content $TxtFilePath
+    $ServerCount = $Servers.count
+}
+
+
+if ( -not (Test-Path $TxtFilePath -PathType Leaf))
 {
     # the servers.txt file wasn't found, write error and exit
-    "No server text file found. Please create a Servers.txt at c:\temp\diskspacereports\ or edit script to look at desired path."
+    "No server text file found. Please create a Servers.txt at c:\temp\diskspacereports\ or edit $TxtFilePath to desired path."
     exit
 }
     else {
-        # servers.txt exists, grab it
-        $File = Get-Content -Path C:\temp\diskspacereports\servers.txt
-        Write-Host "Retrieving contents of servers.txt"
+        # txt file exists, grab it
+        $File = Get-Content -Path $TxtFilePath
+        Write-Host "Retrieving contents of txt file"
         Start-Sleep -Seconds 4
     }
 
-function Get-Servers {
-    $Servers = Get-Content C:\temp\diskspacereports\servers.txt
-    $ServerCount = $Servers.count
-    Write-Host "Found " $ServerCount "server(s) in text file"
-    Start-Sleep -Seconds 4  
-}
+
+Start-Sleep -Seconds 4
 Get-Servers
 
 $RunAccount = Get-Credential -Message "Enter admin account username and password"
 $LogDate = get-date -f yyyy_MM_dd_hhmm
-$DiskReport = ForEach ($Servernames in ($File))
 
+Write-Host "Scanning disks"
+
+$DiskReport = ForEach ($Servernames in ($File))
 
 {Get-WmiObject win32_logicaldisk -Credential $RunAccount `
 -ComputerName $Servernames -Filter "Drivetype=3" `
 -ErrorAction SilentlyContinue }
 
+Write-Host "Scanning disks complete, creating csv file"
+
 #create reports
 $DiskReport | 
 Select-Object @{Label = "Server Name";Expression = {$_.SystemName}},
 @{Label = "Drive Letter";Expression = {$_.DeviceID}},
-@{Label = "Total Capacity (MB)";Expression = {"{0:N1}" -f( $_.Size / 1mb)}},
-@{Label = "Free Space (MB)";Expression = {"{0:N1}" -f( $_.Freespace / 1mb ) }},
+@{Label = "Total Capacity (GB)";Expression = {"{0:N1}" -f( $_.Size / 1gb)}},
+@{Label = "Free Space (GB)";Expression = {"{0:N1}" -f( $_.Freespace / 1gb ) }},
 @{Label = 'Free Space (%)'; Expression = {"{0:P0}" -f ($_.freespace/$_.size)}} |
 
 #Export report to CSV file (Disk Report)
